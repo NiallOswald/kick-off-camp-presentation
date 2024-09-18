1813a27c09ba998429166fdfead7911460f0115e
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-09-18 23:39:29 +0100

    Add single figure plotting

--- scripts/finite-difference.py
+++ scripts/finite-difference.py
@@ -18,10 +18,12 @@ if __name__ == "__main__":
 
     parser = ArgumentParser(
         description="""Solve a Poisson problem on the unit square.""")
-    parser.add_argument("--analytic", action="store_true",
-                        help="Plot the analytic solution instead of solving the finite element problem.")
+    parser.add_argument("--animation", action="store_true",
+                        help="Produce an animation of the solution across time steps.")
     parser.add_argument("--error", action="store_true",
                         help="Plot the error instead of the solution.")
+    parser.add_argument("--cplot", action="store_true",
+                        help="Produce a color plot of the solution across time steps.")
     parser.add_argument("max_step", type=int, nargs=1,
                         help="The maximum number of time steps.")
     parser.add_argument("time_step", type=float, nargs=1,
@@ -35,14 +37,15 @@ if __name__ == "__main__":
     time_step = args.time_step[0]
     wave_speed = args.wave_speed[0]
     resolution = args.resolution[0]
-    analytic = args.analytic
+    animation = args.animation
     plot_error = args.error
+    cplot = args.cplot
 
     # Generate the initial conditions.
     if plot_error:
         u_0 = lambda x, y: cosine(x, y, wave_speed)
     else:
-        u_0 = lambda x, y: bump_function(x, y, 0.5, 0.5, 0.1)
+        u_0 = lambda x, y: bump_function(x, y, 0.5, 0.5, 0.25)
     u_1 = u_0
 
     # Create solver object.
@@ -76,6 +79,31 @@ if __name__ == "__main__":
 
         plt.show()
 
+    elif animation:
+        if cplot:
+            solver.cplot(frames=max_step)
+        else:
+            solver.animate(frames=max_step)
+
     else:
-        # solver.animate(frames=max_step)
-        solver.cplot(frames=max_step)
+        # Iterate numerical solution.
+        with alive_bar(max_step, title="Iterating...") as bar: 
+            solver.advance(max_step, bar)
+        (xx, yy), values = solver.evaluate()
+
+        # Plot the solution.
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="3d")
+
+        plots = [ax.plot_surface(xx, yy, values, cmap="viridis")]
+
+        ax.set_xlabel(r"$x$")
+        ax.set_ylabel(r"$y$")
+        ax.set_zlabel(r"$\eta$")
+
+        ax.set_zlim(-1, 1)
+
+        fig.tight_layout()
+
+        # Save the figure.
+        plt.savefig("../figures/fdm-solution.png", dpi=300)
--- scripts/finite-element.py
+++ scripts/finite-element.py
@@ -12,6 +12,7 @@ from wave_solvers import FiniteElementWaveEquation
 from wave_solvers.utils import bump_function, cosine
 
 import matplotlib.pyplot as plt
+from matplotlib import colors
 from matplotlib.tri import Triangulation
 
 
@@ -19,10 +20,12 @@ if __name__ == "__main__":
 
     parser = ArgumentParser(
         description="""Solve a Poisson problem on the unit square.""")
-    parser.add_argument("--analytic", action="store_true",
-                        help="Plot the analytic solution instead of solving the finite element problem.")
+    parser.add_argument("--animation", action="store_true",
+                        help="Produce an animation of the solution across time steps.")
     parser.add_argument("--error", action="store_true",
                         help="Plot the error instead of the solution.")
+    parser.add_argument("--cplot", action="store_true",
+                        help="Produce a color plot of the solution across time steps.")
     parser.add_argument("max_step", type=int, nargs=1,
                         help="The maximum number of time steps.")
     parser.add_argument("time_step", type=float, nargs=1,
@@ -39,14 +42,15 @@ if __name__ == "__main__":
     wave_speed = args.wave_speed[0]
     resolution = args.resolution[0]
     degree = args.degree[0]
-    analytic = args.analytic
+    animation = args.animation
     plot_error = args.error
+    cplot = args.cplot
 
     # Generate the initial conditions.
     if plot_error:
         u_0 = lambda x: cosine(x[0], x[1], wave_speed)
     else:
-        u_0 = lambda x: bump_function(x[0], x[1], 0.5, 0.5, 0.1)
+        u_0 = lambda x: bump_function(x[0], x[1], 0.5, 0.5, 0.25)
     u_1 = u_0
 
     # Create solver object.
@@ -91,6 +95,35 @@ if __name__ == "__main__":
         ax.set_title("Error")
         plt.show()
     
+    elif animation:
+        if cplot:
+            solver.cplot(frames=max_step)
+        else:
+            solver.animate(frames=max_step)
+
     else:
-        solver.animate(frames=max_step)
-        # solver.cplot(frames=max_step)
+        # Iterate numerical solution.
+        with alive_bar(max_step, title="Iterating...") as bar: 
+            solver.advance(max_step, bar)
+        coords, values, triangles = solver.evaluate(return_triangles=True)
+
+        # Plot the solution.
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="3d")
+
+        norm = colors.Normalize(vmin=values.min(), vmax=values.max())
+        
+        for c in range(coords.shape[0]):
+            ax.plot_trisurf(Triangulation(coords[c, :, 0], coords[c, :, 1], triangles),
+                                          values[c, :], norm=norm, cmap="viridis", linewidth=0)
+
+        ax.set_xlabel(r"$x$")
+        ax.set_ylabel(r"$y$")
+        ax.set_zlabel(r"$\eta$")
+
+        ax.set_zlim(-1, 1)
+
+        fig.tight_layout()
+
+        # Save the figure.
+        plt.savefig("../figures/fem-solution.png", dpi=300)
